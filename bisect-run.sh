f7e0ae744c79612fa53ecf996cadcbc3c0ae9d7f
Tony Kelman <tony@kelman.net>
2017-01-02 07:30:12 -0500

    problem only shows up in release builds

--- bisect-run.sh
+++ bisect-run.sh
@@ -10,10 +10,11 @@ cp /usr/i686-w64-mingw32/sys-root/mingw/bin/*.dll bin
 echo 'set(CMAKE_C_COMPILER cc)' > NATIVE.cmake
 echo 'set(CMAKE_CXX_COMPILER c++)' >> NATIVE.cmake
 cmake /opt/llvm/src -DCMAKE_SYSTEM_NAME=Windows \
+  -DCMAKE_BUILD_TYPE=Release \
   -DCROSS_TOOLCHAIN_FLAGS_NATIVE=-DCMAKE_TOOLCHAIN_FILE=$PWD/NATIVE.cmake \
   -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc \
   -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ \
-  -DLLVM_TARGETS_TO_BUILD=host > llvm-conf.log 2>&1 || exit 125
+  -DLLVM_TARGETS_TO_BUILD=host > llvm-conf.log || exit 125
 make -j`nproc` ONLY_TOOLS=opt > llvm-build.log || exit 125
 ! wine /opt/llvm/build/bin/opt.exe -slp-vectorizer \
   -S /opt/llvm/src/test/Transforms/SLPVectorizer/X86/vector.ll
